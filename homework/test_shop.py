665f34f490413db210e63b9db5b99ddad64a1505
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-09 15:01:20 +0300

    Добавлены тесты по покупке

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -28,7 +28,9 @@ class TestProducts:
         assert product.check_quantity(product.quantity)
 
     def test_product_buy_is_enough(self, product):
-        assert product.buy(product.quantity)
+        quantity_before = product.quantity
+        product.buy(100)
+        assert product.quantity ==quantity_before-100
 
     def test_product_buy_more_than_available(self, product):
         with pytest.raises(ValueError):
@@ -59,7 +61,7 @@ class TestCart:
 
     def test_add_more_product(self, cart, product):
         with pytest.raises(ValueError):
-            cart.add_product(product, product.quantity+1)
+            cart.add_product(product, product.quantity + 1)
 
     def test_add_zero_product(self, cart, product):
         with pytest.raises(ValueError):
@@ -71,9 +73,9 @@ class TestCart:
         assert cart.products[product] == 100 and cart.products[product2] == 100
 
     def test_remove_none_product(self, cart, product):
-        cart.add_product(product,100)
+        cart.add_product(product, 100)
         cart.remove_product(product)
-        assert cart.products =={}
+        assert cart.products == {}
 
     def test_remove_all_product(self, cart, product):
         q = 100
@@ -83,26 +85,96 @@ class TestCart:
 
     def test_remove_more_product(self, cart, product):
         cart.add_product(product, product.quantity)
-        cart.remove_product(product, remove_count=cart.products[product]+1)
+        cart.remove_product(product, remove_count=cart.products[product] + 1)
         assert cart.products == {}
 
     def test_remove_less_product(self, cart, product):
-        q=20
+        q = 20
         cart.add_product(product, q)
-        cart.remove_product(product, remove_count=q-1)
-        assert cart.products[product] == q-(q-1)
+        cart.remove_product(product, remove_count=q - 1)
+        assert cart.products[product] == q - (q - 1)
 
     def test_remove_one_of_product(self, cart, product, product2):
-        q=20
+        q = 20
         cart.add_product(product, q)
         cart.add_product(product2, q)
-        cart.remove_product(product, remove_count=q-1)
-        assert cart.products[product] == q-(q-1)
+        cart.remove_product(product, remove_count=q - 1)
+        assert cart.products[product] == q - (q - 1)
         assert cart.products[product2] == q
 
     def test_remove_another_product(self, cart, product, product2):
         with pytest.raises(ValueError):
             q = 20
             cart.add_product(product, q)
-            cart.remove_product(product2, remove_count=q-1)
+            cart.remove_product(product2, remove_count=q - 1)
+
+    def test_clear_cart(self, cart, product):
+        cart.add_product(product, 100)
+        cart.clear()
+        # assert cart.products == {}
+        assert not cart.products
+
+    def test_clear_empty_cart(self, cart, product):
+        cart.clear()
+        # assert cart.products == {}
+        assert not cart.products
+
+    def test_total_price_one_product(self, cart, product):
+        q_product = 100
+        cart.add_product(product, 100)
+        cart.get_total_price()
+        total_price = cart.get_total_price()
+        assert total_price == product.price * q_product
+
+
+    def test_total_price_two_products(self, cart, product, product2):
+        q_product = 100
+        q_product2 = 100
+        cart.add_product(product, 100)
+        cart.add_product(product2, 100)
+        cart.get_total_price()
+        total_price = cart.get_total_price()
+        assert total_price == product.price * q_product + product2.price * q_product2
+
+
+    def test_buy_product(self, cart, product):
+        product_in_storage = product.quantity
+        q_product = 100
+        cart.add_product(product, q_product)
+        cart.buy()
+        assert product.quantity == product_in_storage - q_product
+        assert not cart.products
+
+
+    def test_buy_two_product(self, cart, product,product2):
+        product_in_storage = product.quantity
+        product2_in_storage = product2.quantity
+        q_product = 100
+        q_product2 = 100
+        cart.add_product(product, q_product)
+        cart.add_product(product2, q_product2)
+        cart.buy()
+
+        assert product.quantity == product_in_storage - q_product
+        assert product2.quantity == product2_in_storage - q_product2
+        assert not cart.products
+
+
+    def test_buy_over_product(self, cart, product):
+        cart.add_product(product, product.quantity)
+        cart.products[product] += 1
+        with pytest.raises(ValueError):
+            cart.buy()
+
+    def test_buy_zero_product(self, cart, product):
+        product_in_storage = product.quantity
+        cart.add_product(product, 1)
+        cart.products[product] = 0
+        cart.buy()
+        assert product.quantity == product_in_storage
+
+
+
+
+
 
